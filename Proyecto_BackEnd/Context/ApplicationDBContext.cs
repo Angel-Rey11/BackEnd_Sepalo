19d21a34065cd2d0932d1c359b0a3ceae238fd99
Angel-Rey11 <91887811+Angel-Rey11@users.noreply.github.com>
2023-02-08 17:18:45 +0100

    backend .net

---
+++ Proyecto_BackEnd/Context/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto_BackEnd.Model;
+
+namespace Proyecto_BackEnd.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { 
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
---
+++ Proyecto_BackEnd/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto_BackEnd.Context;
+using Proyecto_BackEnd.Controllers;
+using Proyecto_BackEnd.Repository;
+using Proyecto_BackEnd.Service;
+using System.Configuration;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<UserController>();
+
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
